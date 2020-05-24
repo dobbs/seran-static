bb86ab73095b34fac5ebdb90ddb38a6a2f51e75d
Eric Dobbs <edobbs@newrelic.com>
2020-05-24 16:49:18 -0600

    use template in pagefold

--- client/pagefold.js
+++ client/pagefold.js
@@ -1,37 +1,30 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+:host {
+  display: block;
+  height: 10px;
+  border-top: 2px solid lightgray;
+  margin-top: 24px;
+  text-align: center;
+  position: relative;
+  clear: both;
+}
+slot {
+  position: relative;
+  top: -.8em;
+  background: white;
+  display: inline-block;
+  color: gray;
+}
+</style>
+<slot></slot>`;
 export class PageFold extends HTMLElement {
-  connectedCallback() {
-    if (this.inited) return;
-    this.inited = true;
-    let shadow = this.attachShadow({ mode: "open" });
-    let css = `
-            :host {
-                display: block;
-                height: 10px;
-                border-top: 2px solid lightgray;
-                margin-top: 24px;
-                text-align: center;
-                position: relative;
-                clear: both;
-            }
-
-            span {
-                position: relative;
-                top: -.8em;
-                background: white;
-                display: inline-block; 
-                color: gray;
-            }
-        `;
-    let style = document.createElement("style");
-    style.innerHTML = css;
-    shadow.appendChild(style);
-    let span = document.createElement("span");
-    span.light = this;
-    shadow.appendChild(span);
-    let slot = document.createElement("slot");
-    span.appendChild(slot);
+  constructor() {
+    super();
+    this.attachShadow({mode: "open"});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
-
   set json(json) {
     // Avoid use of innerHTML by using unicode escape for non-breaking space
     this.textContent = `\u00a0 ${json.text} \u00a0`;
